af87b7be5fb7d0b3d2557e283f2ae30a42c1a6c0
André Oliveira <168911143+oliveirarick@users.noreply.github.com>
2024-07-13 12:35:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+age=float(input("Enter your age: "))
+
+if age >= 18:
+  value = 18.40
+  print(f"You will pay in full. (Full Value=U$18.40)")
+
+else:
+  value = 9.40
+  print(f"You will pay half. (Half Value=U$9.40)")
